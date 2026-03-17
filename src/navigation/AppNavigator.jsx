32ab9b3458f12f78f2244f341b3ee24f6dd2a07e
XuanThanh05 <xuanthanhle999@gmail.com>
2026-03-17 17:35:20 +0700

    setup

---
+++ src/navigation/AppNavigator.jsx
@@ -0,0 +1,29 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// import screens
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+        />
+
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+        />
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
